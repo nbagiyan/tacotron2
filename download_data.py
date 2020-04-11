6427479e7bd61ca506caed80936ac3e53174b813
BAGIYAN Nerses <Nerses.BAGIYAN@raiffeisen.ru>
2020-04-11 14:47:58 +0300

    fix bug

--- download_data.py
+++ download_data.py
@@ -3,10 +3,10 @@ import os
 links = [
     "https://www.dropbox.com/s/s3ahqeggg9muba2/wav_tts_part1.zip",
     "https://www.dropbox.com/s/q0809p68sbqtkvr/wav_tts_part2.zip",
-    # "https://www.dropbox.com/s/acd41gahowcbycf/wav_tts_part3.zip",
-    # "https://www.dropbox.com/s/q5qtpc9x5wcewab/wav_tts_part4.zip",
-    # "https://www.dropbox.com/s/uwcscnb8gvkx175/wav_tts_part5.zip",
-    # "https://www.dropbox.com/s/96rquckkerwvseo/wav_tts_part6.zip",
+    "https://www.dropbox.com/s/acd41gahowcbycf/wav_tts_part3.zip",
+    "https://www.dropbox.com/s/q5qtpc9x5wcewab/wav_tts_part4.zip",
+    "https://www.dropbox.com/s/uwcscnb8gvkx175/wav_tts_part5.zip",
+    "https://www.dropbox.com/s/96rquckkerwvseo/wav_tts_part6.zip",
     # "https://www.dropbox.com/s/4gq4c334e6w59a5/wav_tts_part7.zip",
     # "https://www.dropbox.com/s/lonm42nyv0f8jse/wav_tts_part8.zip"
 ]
@@ -16,16 +16,16 @@ def walk_dir_and_write(dir1, dir2, f):
     for file in os.listdir(f"./data/{dir1}/{dir2}"):
         if '.wav' in file:
             text_path = f"./data/{dir1}/{dir2}/{file.split('.')[0] + '.txt'}"
-
-            with open(text_path, 'r') as txt:
-                text = txt.read().strip()
-            f.write(f"./data/{dir1}/{dir2}/{file}|{text}\n")
+            if os.path.exists(text_path):
+                with open(text_path, 'r') as txt:
+                    text = txt.read().strip()
+                f.write(f"./data/{dir1}/{dir2}/{file}|{text}\n")
 
 
 if __name__ == '__main__':
     os.system("mkdir data")
     for link in links:
-        # os.system(f"wget {link}")
+        os.system(f"wget {link}")
         os.system(f"unzip {link.split('/')[-1]} -d data")
         os.system(f"rm -rf {link.split('/')[-1]}")
 
