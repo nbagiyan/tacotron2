f4062b853ff6740f6ee8ec86e3137e7c7f208ea3
BAGIYAN Nerses <Nerses.BAGIYAN@raiffeisen.ru>
2020-04-11 15:35:43 +0300

    fix bug with length

--- model.py
+++ model.py
@@ -505,7 +505,7 @@ class Tacotron2(nn.Module):
         try:
             encoder_outputs = self.encoder(embedded_inputs, text_lengths)
         except:
-            print(text_lengths,)
+            print(text_lengths, torch.all(text_lengths > 0))
             raise
         gst_outputs = self.gst(mels)
         gst_outputs = gst_outputs.expand_as(encoder_outputs)
