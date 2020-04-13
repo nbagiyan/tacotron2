7ee7efce6f53fd10aa0aa78e9b7072900c596bf7
BAGIYAN Nerses <Nerses.BAGIYAN@raiffeisen.ru>
2020-04-13 20:05:02 +0300

    change download file

--- download_data.py
+++ download_data.py
@@ -20,7 +20,7 @@ def walk_dir_and_write(dir1, dir2, f):
             if os.path.exists(text_path):
                 with open(text_path, 'r') as txt:
                     text = txt.read().strip()
-                    _, data = read('./data/' + file)
+                    _, data = read(f"./data/{dir1}/{dir2}/{file}")
                     if len(text) > 15 and len(data) != 0:
                         f.write(f"./data/{dir1}/{dir2}/{file}|{text}\n")
 
