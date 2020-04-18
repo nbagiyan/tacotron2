89f27d313cd0529dfc863fa63f1651525bfd726a
BAGIYAN Nerses <Nerses.BAGIYAN@raiffeisen.ru>
2020-04-18 16:44:13 +0300

    change hparams

--- hparams.py
+++ hparams.py
@@ -24,7 +24,7 @@ def create_hparams(hparams_string=None, verbose=False):
         ################################
         # Data Parameters             #
         ################################
-        load_mel_from_disk=True,
+        load_mel_from_disk=False,
         training_files='train.txt',
         validation_files='val.txt',
         text_cleaners=['basic_cleaners'],
