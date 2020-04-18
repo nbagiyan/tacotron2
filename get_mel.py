a8ea09b3d5a2d10bf33f48c6a1419cb8ddc73034
BAGIYAN Nerses <Nerses.BAGIYAN@raiffeisen.ru>
2020-04-18 17:32:45 +0300

    change get_mel

--- get_mel.py
+++ get_mel.py
@@ -18,3 +18,38 @@ def get_mel(hparams, path):
     melspec = stft.mel_spectrogram(audio_norm)
     melspec = torch.squeeze(melspec, 0)
     return melspec
+
+
+def collate(batch, hparams):
+    input_lengths, ids_sorted_decreasing = torch.sort(
+        torch.LongTensor([len(x[0]) for x in batch]),
+        dim=0, descending=True)
+    max_input_len = input_lengths[0]
+
+    text_padded = torch.LongTensor(len(batch), max_input_len)
+    text_padded.zero_()
+    for i in range(len(ids_sorted_decreasing)):
+        text = batch[ids_sorted_decreasing[i]][0]
+        text_padded[i, :text.size(0)] = text
+
+    # Right zero-pad mel-spec
+    num_mels = batch[0][1].size(0)
+    max_target_len = max([x[1].size(1) for x in batch])
+    if max_target_len % hparams.n_frames_per_step != 0:
+        max_target_len += hparams.n_frames_per_step - max_target_len % hparams.n_frames_per_step
+        assert max_target_len % hparams.n_frames_per_step == 0
+
+    # include mel padded and gate padded
+    mel_padded = torch.FloatTensor(len(batch), num_mels, max_target_len)
+    mel_padded.zero_()
+    gate_padded = torch.FloatTensor(len(batch), max_target_len)
+    gate_padded.zero_()
+    output_lengths = torch.LongTensor(len(batch))
+    for i in range(len(ids_sorted_decreasing)):
+        mel = batch[ids_sorted_decreasing[i]][1]
+        mel_padded[i, :, :mel.size(1)] = mel
+        gate_padded[i, mel.size(1) - 1:] = 1
+        output_lengths[i] = mel.size(1)
+
+    return text_padded, input_lengths, mel_padded, gate_padded, \
+           output_lengths
