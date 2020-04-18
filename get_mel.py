9887abfd9b0697c8f0714f95140e95d031691af0
BAGIYAN Nerses <Nerses.BAGIYAN@raiffeisen.ru>
2020-04-18 12:24:15 +0300

    add inference part

---
+++ get_mel.py
@@ -0,0 +1,20 @@
+import torch
+import layers
+from utils import load_wav_to_torch
+
+
+def get_mel(hparams, path):
+    stft = layers.TacotronSTFT(
+            hparams.filter_length, hparams.hop_length, hparams.win_length,
+            hparams.n_mel_channels, hparams.sampling_rate, hparams.mel_fmin,
+            hparams.mel_fmax)
+    audio, sampling_rate = load_wav_to_torch(path)
+    if sampling_rate != stft.sampling_rate:
+        raise ValueError("{} {} SR doesn't match target {} SR".format(
+            sampling_rate, stft.sampling_rate))
+    audio_norm = audio / hparams.max_wav_value
+    audio_norm = audio_norm.unsqueeze(0)
+    audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+    melspec = stft.mel_spectrogram(audio_norm)
+    melspec = torch.squeeze(melspec, 0)
+    return melspec
--- model.py
+++ model.py
@@ -517,9 +517,13 @@ class Tacotron2(nn.Module):
             [mel_outputs, mel_outputs_postnet, gate_outputs, alignments],
             output_lengths)
 
-    def inference(self, inputs):
+    def inference(self, inputs, mels):
         embedded_inputs = self.embedding(inputs).transpose(1, 2)
         encoder_outputs = self.encoder.inference(embedded_inputs)
+        gst_outputs = self.gst(mels)
+        gst_outputs = gst_outputs.expand_as(encoder_outputs)
+        encoder_outputs += gst_outputs
+
         mel_outputs, gate_outputs, alignments = self.decoder.inference(
             encoder_outputs)
 
