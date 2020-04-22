9f44a9cdbf02c095e6611815152bb73827a9ffb5
BAGIYAN Nerses <Nerses.BAGIYAN@raiffeisen.ru>
2020-04-22 18:54:36 +0300

    fix bug with batch first

--- gst.py
+++ gst.py
@@ -13,18 +13,18 @@ class ReferenceEncoder(nn.Module):
     def __init__(self, hp):
 
         super().__init__()
-        K = len(hp.ref_enc_filters)
+        self.K = len(hp.ref_enc_filters)
         filters = [1] + hp.ref_enc_filters
 
         convs = [nn.Conv2d(in_channels=filters[i],
                            out_channels=filters[i + 1],
                            kernel_size=(3, 3),
                            stride=(2, 2),
-                           padding=(1, 1)) for i in range(K)]
+                           padding=(1, 1)) for i in range(self.K)]
         self.convs = nn.ModuleList(convs)
-        self.bns = nn.ModuleList([nn.BatchNorm2d(num_features=hp.ref_enc_filters[i]) for i in range(K)])
+        self.bns = nn.ModuleList([nn.BatchNorm2d(num_features=hp.ref_enc_filters[i]) for i in range(self.K)])
 
-        out_channels = self.calculate_channels(hp.n_mels, 3, 2, 1, K)
+        out_channels = self.calculate_channels(hp.n_mels, 3, 2, 1, self.K)
         self.gru = nn.GRU(input_size=hp.ref_enc_filters[-1] * out_channels,
                           hidden_size=hp.E // 2,
                           batch_first=True)
